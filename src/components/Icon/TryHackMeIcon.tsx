aababa418c4be7779ae9c24f1cb86d0fd02f7aa5
Morgan Davies <morgandavies2020@gmail.com>
2023-03-22 13:46:35 +0000

    Fix linting issues

--- src/components/Icon/TryHackMeIcon.tsx
+++ src/components/Icon/TryHackMeIcon.tsx
@@ -1,7 +1,7 @@
 import {FC, memo} from "react";
 
 import {IconProps} from "./Icon";
-import { Icon } from '@iconify/react';
+import {Icon} from '@iconify/react';
 
 const TryHackMeIcon: FC<IconProps> = memo(() => (
   <Icon icon="simple-icons:tryhackme" width="26" height="26" />
